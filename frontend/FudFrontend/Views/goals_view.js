f19a49d484890c71cb67ba7b7c2552a04f52e75e
Pranav Upadhyayula <pranavupadhyayula@DN0a22a9e0.SUNet>
2020-02-12 15:58:01 -0800

    added a lot of fitness goal components

--- frontend/FudFrontend/Views/goals_view.js
+++ frontend/FudFrontend/Views/goals_view.js
@@ -5,8 +5,14 @@ import {
     View,
     StyleSheet,
     SafeAreaView,
+    ScrollView,
     Text,
+    TextInput,
+    Picker,
+    KeyboardAvoidingView
 } from 'react-native';
+import Constants from 'expo-constants';
+import {Slider} from 'react-native'
 import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 import { styles } from '../Styles/styles'
 
@@ -17,7 +23,10 @@ function Separator() {
 export class GoalsScreen extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {fitness_goal: 0};
+      this.state = {
+        fitness_goal: 0,
+        activity_level: "Sedentary"
+      };
     }
 
     static navigationOptions = {
@@ -29,37 +38,111 @@ export class GoalsScreen extends React.Component {
 
       let radio_props_fitness_goals = [
         {label: 'Fat loss', value: 0 },
-        {label: 'Muscle gain', value: 1 }
+        {label: 'Muscle gain', value: 1 },
+        {label: 'Maintenance', value: 2 }
       ];
 
       return (
-        <SafeAreaView style={styles.container}>
-          <View style={styles.container}>
-            <Text>
-              Set your fitness goals:
-            </Text>
+        <SafeAreaView style={styles_local.container}>
+          <ScrollView>
+            <View style={styles_local.container}>
+              <Text style={styles_local.title}>
+                About you:
+              </Text>
 
-            <RadioForm
-              radio_props={radio_props_fitness_goals}
-              initial={0}
-              onPress={(value) => {this.setState({fitness_goal:value})}}
-            />
+              <TextInput
+                style={{ height: 40, borderWidth: 1 }}
+                placeholder = 'Height (in)'
+              />
 
-            <Text>
-              You selected: {radio_props_fitness_goals[this.state.fitness_goal].label}
-            </Text>
+              <TextInput
+                style={{ height: 40, borderWidth: 1 }}
+                placeholder = 'Weight (lbs)'
+              />
 
-          </View>
+              <Text style={styles_local.title}>
+                Activity level:
+              </Text>
 
-          <Separator />
+              <Picker
+                selectedValue={this.state.activity_level}
+                onValueChange={(itemValue, itemIndex) =>
+                  this.setState({activity_level: itemValue})
+                }>
+                <Picker.Item label="Sedentary" value="Sedentary" />
+                <Picker.Item label="Moderate" value="Moderate" />
+                <Picker.Item label="Athlete" value="Athlete" />
+              </Picker>
+              <Text style={styles_local.title}>
+                You selected: {this.state.activity_level}
+              </Text>
+            </View>
 
-          <View style={styles.container}>
-            <Button
-              title="Set Füd Preferences"
-              onPress={this._setFudPrefsAsync}
-            />
-            <StatusBar barStyle="default" />
-          </View>
+            <Separator />
+
+            <View style={styles_local.container}>
+              <Text style={styles_local.title}>
+                Set your fitness goals:
+              </Text>
+
+              <RadioForm
+                radio_props={radio_props_fitness_goals}
+                initial={0}
+                onPress={(value) => {this.setState({fitness_goal:value})}}
+                style={styles_local.title}
+              />
+
+              <Text style={styles_local.title}>
+                You selected: {radio_props_fitness_goals[this.state.fitness_goal].label}
+              </Text>
+
+              <Text style={styles_local.title}>
+                Details:
+              </Text>
+
+              <Slider
+                style={{width: 200, height: 40}}
+                minimumValue={0}
+                maximumValue={10}
+                minimumTrackTintColor="#3B821B"
+                maximumTrackTintColor="#000000"
+              />
+
+              <TextInput
+                style={{ height: 40, borderWidth: 1 }}
+                placeholder = 'Number of pounds to lose/gain'
+              />
+              <TextInput
+                style={{ height: 40, borderWidth: 1 }}
+                placeholder = 'Number of weeks to achieve goal'
+              />
+            </View>
+
+            <Separator />
+
+            <View style={styles.container}>
+              <Text style={styles_local.title}>
+                Calculations:
+              </Text>
+              <Text>
+                Eat *1700* calories per day, or *11,900* calories per week.
+              </Text>
+              <Text>
+                Your diet should consist of *40%* carbs, *30%* protein, and *30%* fat.
+              </Text>
+              <Text>
+                This means *170g* carbs, *128g* protein, and *57g* fat on a daily basis.
+              </Text>
+            </View>
+
+            <View style={styles.container}>
+              <Button
+                title="Set Füd Preferences"
+                onPress={this._setFudPrefsAsync}
+              />
+              <StatusBar barStyle="default" />
+            </View>
+          </ScrollView>
         </SafeAreaView>
       );
     }
@@ -70,9 +153,22 @@ export class GoalsScreen extends React.Component {
   }
 
   const styles_local = StyleSheet.create({
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#000000',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
+    container: {
+      flex: 1,
+      marginTop: Constants.statusBarHeight,
+      marginHorizontal: 16,
+    },
+    title: {
+      textAlign: 'center',
+      marginVertical: 8,
+    },
+    fixToText: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    separator: {
+      marginVertical: 8,
+      borderBottomColor: '#737373',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+    },
 });
