f72b8637351e1a36b47adef5884c2d20f0916ada
Ben Rocklin <brocklin@cs.stanford.edu>
2020-02-18 00:12:01 -0800

    Basic backend auth

--- backend/auth.py
+++ backend/auth.py
@@ -1,15 +1,79 @@
-from app import app
+from app import app, auth
 from flask import request, jsonify
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+from itsdangerous import Signer
+from passlib.hash import pbkdf2_sha256
+import pymongo
 
-@app.route('/login', methods=['GET'])
+client = pymongo.MongoClient("mongodb+srv://connor:connor@foodcluster-trclg.mongodb.net/test?retryWrites=true&w=majority")
+db = client.users.users_credentials
+
+@app.route('/api/register', methods=['POST'])
+def register_auth():
+  username = request.json.get('username')
+  password = request.json.get('password')
+  if username is None or password is None:
+    return "Please include a username and password", 400
+  if db.find_one({"username": username}) is not None:
+    return "Cannot overwrite existing user", 400
+  new_user = {"username": username}
+  new_user["password"] = hash_pwd(password)
+  db.insert_one(new_user)
+  return jsonify({"username": username}, 201)
+
+@app.route('/api/login', methods=['POST'])
+@auth.login_required
 def login_auth():
-  user, pwd = "",""
   results = {"success": True}
-  if "username" in request.args:
-    user = request.args["username"]
-    results["user"] = user
-  if "password" in request.args:
-    pwd = request.args["password"]
-  if pwd != "EEE":
-    results["success"] = False
   return jsonify(results)
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+  return jsonify({ 'data': 'Password test' })
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+  username = request.authorization['username']
+  if username is None:
+    return "Need username/pwd for token", 401
+  token = generate_auth_token(request.authorization['username'])
+  return jsonify({ 'token': token.decode('ascii') })
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+  print(request.headers)
+  username = verify_auth_token(username_or_token)
+  if not username:
+    username = username_or_token
+    user = db.find_one({"username": username})
+    if not user or not verify_pwd(password, user["password"]):
+      return False
+  return True
+
+def hash_pwd(password):
+  return pbkdf2_sha256.hash(password)
+
+def verify_pwd(passwordA, passwordB):
+  return pbkdf2_sha256.verify(passwordA, passwordB)
+
+def generate_auth_token(username, expiration = 1000):
+  s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+  token = s.dumps({ 'username': username})
+  print(token.decode('ascii'))
+  return token
+
+def verify_auth_token(token):
+  print(token)
+  s = Serializer(app.config['SECRET_KEY'])
+  try:
+    data = s.loads(token)
+  except SignatureExpired:
+    return None
+  except BadSignature:
+    return None
+  return data['username']
+  
+  
