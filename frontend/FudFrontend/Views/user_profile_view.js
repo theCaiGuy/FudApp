cbe70b39156c68661c9f6d62e58ebc58b1704b67
theCaiGuy <michaelcai09@gmail.com>
2020-02-24 15:57:56 -0800

    user profile screen boilerplate added

---
+++ frontend/FudFrontend/Views/user_profile_view.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  AsyncStorage,
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview'
+import { styles } from '../Styles/styles'
+import {
+  Button,
+  ButtonGroup,
+  Input,
+} from 'react-native-elements'
+import { API_PATH } from '../assets/constants'
+import {encode as btoa} from 'base-64'
+
+
+export class UserProfileScreen extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        user_id: None
+      };
+    }
+
+    render() {
+
+      return (
+        <SafeAreaView style={styles.container}>
+          <KeyboardAwareScrollView>
+            <View style={styles.container}>
+                <Text style={styles.central_header_text}>User Profile Page</Text>
+            </View>
+          </KeyboardAwareScrollView>
+        </SafeAreaView>
+      );
+    }
+
+  }
